7aef9e7cc7883be8ea650f0fa7ff49e8fafd7812
Chris-Molotov <cbonnin@molotov.tv>
2021-10-04 15:12:33 +0200

    added main package

--- utilis.go
+++ utilis.go
@@ -2,8 +2,13 @@ package main
 
 func main(){
    fmt.Println("test")
+<<<<<<< HEAD
    fmt.Println("Conflits")
    fmt.Println("Learning Git")
    fmt.Println("Rebasing")
 }
 
+=======
+
+}
+>>>>>>> 2b8de08 (added main package)
