8f8c6214889d3bf91ba566f0736733664939b363
Chris Bonnin <73790587+Cbonnin88@users.noreply.github.com>
2021-10-04 16:25:21 +0200

    Feature xyz (#2)
    * added main package
    
    * Added another println
    
    * Update utilis.go
    
    * removed error

--- utilis.go
+++ utilis.go
@@ -2,8 +2,24 @@ package main
 
 func main(){
    fmt.Println("test")
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
    fmt.Println("Conflits")
    fmt.Println("Learning Git")
+<<<<<<< HEAD
    fmt.Println("Rebasing")
 }
 
+=======
+=======
+   fmt.Println("Learning Git")
+>>>>>>> d055d5d (Update utilis.go)
+
+=======
+   fmt.Println("Conflits")
+>>>>>>> be974bb (Added another println)
+=======
+>>>>>>> f340331 (removed error)
+}
+>>>>>>> 2b8de08 (added main package)
